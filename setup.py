7fb823d73c307cb62fa5b620c4925268ff99ef22
Andy Lu <andy@stitchdata.com>
2019-10-24 15:53:53 +0000

    Bump to v0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-chargify",
-    version="0.0.1",
+    version="0.0.2",
     description="Singer.io tap for extracting Chargify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-chargify",
