61b695809e67e2ff242a8348ddfbe5481dc76530
andy <andyjiang@gmail.com>
2019-11-01 16:40:59 -0400

    add pagination;

--- tap_chargify/chargify.py
+++ tap_chargify/chargify.py
@@ -7,6 +7,7 @@ import backoff
 import json
 import requests
 import logging
+from urllib.parse import urlencode
 from requests.auth import HTTPBasicAuth
 from singer import utils
 from datetime import datetime
@@ -24,117 +25,123 @@ class Chargify(object):
     self.uri = "https://{subdomain}.chargify.com/".format(subdomain=subdomain)
 
 
-
   def retry_handler(details):
     logger.info("Received 429 -- sleeping for %s seconds",
                 details['wait'])
 
   # 
-  # The actual `get` request.
+  # The `get` request.
   # 
   
   @backoff.on_exception(backoff.expo,
                         requests.exceptions.HTTPError,
                         on_backoff=retry_handler,
                         max_tries=10)
-  def _get(self, path, stream=True, **kwargs):
+  def get(self, path, stream=True, **kwargs):
     uri = "{uri}{path}".format(uri=self.uri, path=path)
-    logger.info("GET request to {uri}".format(uri=uri))
-    response = requests.get(uri, stream=stream, auth=HTTPBasicAuth(self.api_key, 'x'))
-    response.raise_for_status()
-    return response
+    has_more = True
+    page = 1
+    per_page = 200
+    while has_more:
+      params = {
+        "page": page,
+        "per_page": per_page
+      }
+      for key, value in kwargs.items():
+        params[key] = value
+      final_uri = uri + "?{params}".format(params=urlencode(params))
 
-  #
-  # The common `get` request.
-  #
-  
-  def get(self, path, **kwargs):
-    response = self._get(path, **kwargs)
-    return response.json()
+      logger.info("GET request to {final_uri}".format(final_uri=final_uri))
+      response = requests.get(final_uri, stream=stream, auth=HTTPBasicAuth(self.api_key, 'x'))
+      response.raise_for_status()
+
+      page += 1
+      if len(response.json()) < per_page:
+        has_more = False
+
+      yield response.json()
 
   # 
   # Methods to retrieve data per stream/resource.
   # 
 
   def customers(self, bookmark=None):
-    res = self.get("customers.json")
-    for i in res:
-      yield i["customer"]
+    for i in self.get("customers.json"):
+      for j in i:
+        yield j["customer"]
 
 
   def product_families(self, bookmark=None):
-    res = self.get("product_families.json")
-    for i in res:
-      yield i["product_family"]
+    for i in self.get("product_families.json"):
+      for j in i:
+        yield j["product_family"]
 
 
   def products(self, bookmark=None):
-    res = self.get("product_families.json")
-    for i in res:
-      res_prod_families = self.get("product_families/{product_family_id}/products.json".format(product_family_id=i["product_family"]["id"]))
-      for j in res_prod_families:
-        yield j["product"]
+    for i in self.get("product_families.json"):
+      for k in i:
+        for j in self.get("product_families/{product_family_id}/products.json".format(product_family_id=k["product_family"]["id"])):
+          for l in j:
+            yield l["product"]
 
 
   def price_points(self, bookmark=None):
-    res = self.get("product_families.json")
-    for i in res:
-      res_prod_families = self.get("product_families/{product_family_id}/products.json".format(product_family_id=i["product_family"]["id"]))
-      for j in res_prod_families:
-        res_price_points = self.get("products/{product_id}/price_points.json".format(product_id=j["product"]["id"]))
-        for k in res_price_points["price_points"]:
-          yield k
+    for i in self.get("product_families.json"):
+      for j in i:
+        for k in self.get("product_families/{product_family_id}/products.json".format(product_family_id=j["product_family"]["id"])):
+          for l in k:
+            for o in self.get("products/{product_id}/price_points.json".format(product_id=l["product"]["id"])):
+              yield o
 
 
   def coupons(self, bookmark=None):
-    res = self.get("product_families.json")
-    for i in res:
-      res_prod_families = self.get("product_families/{product_family_id}/coupons.json".format(product_family_id=i["product_family"]["id"]))
-      for j in res_prod_families:
-        yield j["coupon"]
+    for i in self.get("product_families.json"):
+      for k in i:
+        for j in self.get("product_families/{product_family_id}/coupons.json".format(product_family_id=k["product_family"]["id"])):
+          for l in j:
+            yield l["coupon"]
 
 
   def components(self, bookmark=None):
-    res = self.get("product_families.json")
-    for i in res:
-      res_prod_families = self.get("product_families/{product_family_id}/components.json".format(product_family_id=i["product_family"]["id"]))
-      for j in res_prod_families:
-        yield j["component"]
+    for i in self.get("product_families.json"):
+      for k in i:
+        for j in self.get("product_families/{product_family_id}/components.json".format(product_family_id=k["product_family"]["id"])):
+          for l in j:
+            yield l["component"]
 
 
 
   def subscriptions(self, bookmark=None):
-    res = self.get("subscriptions.json?start_datetime={start_datetime}&date_field=updated_at".format(start_datetime=bookmark))
-    for i in res:
-      yield i["subscription"]
+    for i in self.get("subscriptions.json", start_datetime=bookmark, date_field="updated_at"):
+      for j in i:
+        yield j["subscription"]
 
 
   def transactions(self, bookmark=None):
     since_date = utils.strptime_with_tz(bookmark).strftime('%Y-%m-%d')
-    # Need to convert bookmark to date string.
-    res = self.get("transactions.json?since_date={since_date}&direction=asc".format(since_date=since_date))
-    for i in res:
-      yield i["transaction"]
+    for i in self.get("transactions.json", since_date=since_date, direction="asc"):
+      for j in i:
+        yield j["transaction"]
 
 
   def statements(self, bookmark=None):
     settled_since = mktime(utils.strptime_with_tz(bookmark).timetuple())
-    res = self.get("statements.json?sort=settled_at&direction=asc&settled_since={settled_since}".format(settled_since=settled_since))
-    for i in res:
-      yield i["statement"]
+    for i in self.get("statements.json", sort="settled_at", direction="asc", settled_since=settled_since):
+      for j in i:
+        yield j["statement"]
 
 
   def invoices(self, bookmark=None):
     start_date = utils.strptime_with_tz(bookmark).strftime('%Y-%m-%d')
-    res = self.get("invoices.json?start_date={start_date}&direction=asc".format(start_date=start_date))
-    for i in res:
-      yield i["invoice"]
+    for i in self.get("invoices.json", start_date=start_date, direction="asc"):
+      for j in i:
+        yield j["invoice"]
 
 
   def events(self, bookmark=None):
-    res = self.get("events.json")
-    for i in res:
-      yield i["event"]
+    for i in self.get("events.json"):
+      for j in i:
+        yield j["event"]
 
 
 
